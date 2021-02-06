dfea91420cdf7fa26f9813fc840be28acc2813d8
SahBianchi <samantha.sbianchi@gmail.com>
2021-02-06 12:26:27 -0200

    deletando arquivos de back

--- src/sevices/IdGenerator.ts
+++
@@ -1,9 +0,0 @@
-import { v4 } from "uuid";
-
-export default class IdGenerator {
-    static generate() {
-      throw new Error("Method not implemented.");
-    }
-
-    public generateId = (): string => (v4());
-};
