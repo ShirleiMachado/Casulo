5b112c50a7961cae4e16e52cf18938b87e948a9f
araujo-gui <paulaalvesaraujo@outlook.com>
2021-02-06 15:47:23 -0300

    branch Gui

--- Front/js/models/cadastro_mentorados_models.js
+++ Front/js/models/cadastro_mentorados_models.js
@@ -1,13 +1,47 @@
-class CadastroMentorModel {
+class CadastroMentoradoModel {
 
     constructor(){
 
-        this._nome = "";
-        this._email = "";
-        this._senha = "";
-        this._uf = "";
-        this._especialidade = "";
-        this._genero = "";
-        this._grupo = ""
+        this.nome = "";
+        this.email = "";
+        this.senha = "";
+        this.uf = "";
+        this.especialidade = "";
+        this.genero = "";
+        this.grupo = ""
+    };
+
+    buscarMentorado(){
+        let request = new XMLHttpRequest();
+
+        request.addEventListener("load", ()=> {
+            if (request.status == 200){
+
+                let dados = this.processaResponse(request.responseText);
+                this.atualiza(dados);
+            }
+        })
+
+        request.open("GET", " " +this.nome, false)
+        request.send();
     }
+
+    processaResponse(responseText){
+        console.log(responseText)
+        let response = JSON.parse(responseText);
+        return response;
+    }
+
+    atualiza(dados){
+
+        this.nome = "";
+        this.email = "";
+        this.senha = "";
+        this.uf = "";
+        this.especialidade = "";
+        this.genero = "";
+        this.grupo = ""
+    }
+
+
 }
